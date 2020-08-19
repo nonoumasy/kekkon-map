1969d72e4db8e04d065df68006d0388c7aa0e9cf
nonoumasy <nonoumasy@gmail.com>
2020-08-18 20:34:39 -0700

    sdafasdf

--- kekkon.js
+++ kekkon.js
@@ -66,7 +66,7 @@ let locations = {
                     "highlight": "#icon-24-highlight"
                 },
                 "icon": "https://www.gstatic.com/mapspro/images/stock/24-blue.png",
-                "description": "<img src=\"https://lh5.googleusercontent.com/proxy/KBVOOSPFhwJh6CnBKQoCR_xPfAtQrIqeqBLBE-oz4uWvCM0Q5fzuqGnNYINXGMLLDuXAioRSJ7Ad17Cyfp-ZbDvmpY2PCn4\" height=\"200\" width=\"auto\" /><br><br>代官山風の館",
+                "description": "<img src=\"https://lh5.googleusercontent.com/proxy/KBVOOSPFhwJh6CnBKQoCR_xPfAtQrIqeqBLBE-oz4uWvCM0Q5fzuqGnNYINXGMLLDuXAioRSJ7Ad17Cyfp-ZbDvmpY2PCn4\" height=\"200\" width=\"auto\" />",
                 "gx_media_links": "https://lh5.googleusercontent.com/proxy/KBVOOSPFhwJh6CnBKQoCR_xPfAtQrIqeqBLBE-oz4uWvCM0Q5fzuqGnNYINXGMLLDuXAioRSJ7Ad17Cyfp-ZbDvmpY2PCn4"
             }
         },
@@ -89,7 +89,7 @@ let locations = {
                     "highlight": "#icon-163-highlight"
                 },
                 "icon": "https://www.gstatic.com/mapspro/images/stock/163-lightblue.png",
-                "description": "<img src=\"https://lh6.googleusercontent.com/proxy/vGTYxTH7tA5q-zEIfB0DV_YQmFtzwOjbbqzPEnSRUfJyY5phS1H37j-lvA5dWBKEkEYTt0pdSEfLKQGzloGFEZUevg\" height=\"200\" width=\"auto\" /><br><br>山二証券（株）本社",
+                "description": "<img src=\"https://lh6.googleusercontent.com/proxy/vGTYxTH7tA5q-zEIfB0DV_YQmFtzwOjbbqzPEnSRUfJyY5phS1H37j-lvA5dWBKEkEYTt0pdSEfLKQGzloGFEZUevg\" height=\"200\" width=\"auto\" />",
                 "gx_media_links": "https://lh6.googleusercontent.com/proxy/vGTYxTH7tA5q-zEIfB0DV_YQmFtzwOjbbqzPEnSRUfJyY5phS1H37j-lvA5dWBKEkEYTt0pdSEfLKQGzloGFEZUevg"
             }
         },
@@ -112,7 +112,7 @@ let locations = {
                     "highlight": "#icon-163-highlight"
                 },
                 "icon": "https://www.gstatic.com/mapspro/images/stock/163-lightblue.png",
-                "description": "<img src=\"https://lh3.googleusercontent.com/proxy/jXWy5W1EodjTQsCXtiSNcuWQpJAPKXluJIA_Vg50KnOl4KjQ3H3pU8QC6sR164EN1K4baeiZZjK0uafiP_grsg\" /><br><br>タイムズ日本鋼管病院第２",
+                "description": "<img src=\"https://lh3.googleusercontent.com/proxy/jXWy5W1EodjTQsCXtiSNcuWQpJAPKXluJIA_Vg50KnOl4KjQ3H3pU8QC6sR164EN1K4baeiZZjK0uafiP_grsg\" />",
                 "gx_media_links": "https://lh3.googleusercontent.com/proxy/jXWy5W1EodjTQsCXtiSNcuWQpJAPKXluJIA_Vg50KnOl4KjQ3H3pU8QC6sR164EN1K4baeiZZjK0uafiP_grsg"
             }
         },
@@ -135,7 +135,7 @@ let locations = {
                     "highlight": "#icon-163-highlight"
                 },
                 "icon": "https://www.gstatic.com/mapspro/images/stock/163-lightblue.png",
-                "description": "<img src=\"https://wiki.d-addicts.com/images/7/72/KekkonDekinaiOtoko.jpg\" height=\"200\" width=\"auto\" /><br><br>",
+                "description": "<img src=\"https://wiki.d-addicts.com/images/7/72/KekkonDekinaiOtoko.jpg\" height=\"200\" width=\"auto\" />",
                 "gx_media_links": "https://wiki.d-addicts.com/images/7/72/KekkonDekinaiOtoko.jpg"
             }
         },
@@ -158,7 +158,7 @@ let locations = {
                     "highlight": "#icon-163-highlight"
                 },
                 "icon": "https://www.gstatic.com/mapspro/images/stock/163-lightblue.png",
-                "description": "<img src=\"https://lh5.googleusercontent.com/proxy/VSJjHu3FGDGmfUPgYnVPfLxjUwtWbDjEZs5u6L9TLlbjnhmqvwuUZ8MOUowXtGX9ACSZXz6Z4w2ucDT-OQhEhYsojpA\" height=\"200\" width=\"auto\" /><br><br>",
+                "description": "<img src=\"https://lh5.googleusercontent.com/proxy/VSJjHu3FGDGmfUPgYnVPfLxjUwtWbDjEZs5u6L9TLlbjnhmqvwuUZ8MOUowXtGX9ACSZXz6Z4w2ucDT-OQhEhYsojpA\" height=\"200\" width=\"auto\" />",
                 "gx_media_links": "https://lh5.googleusercontent.com/proxy/VSJjHu3FGDGmfUPgYnVPfLxjUwtWbDjEZs5u6L9TLlbjnhmqvwuUZ8MOUowXtGX9ACSZXz6Z4w2ucDT-OQhEhYsojpA"
             }
         },
@@ -181,7 +181,7 @@ let locations = {
                     "highlight": "#icon-125-highlight"
                 },
                 "icon": "https://www.gstatic.com/mapspro/images/stock/125-restaurant.png",
-                "description": "<img src=\"https://lh3.googleusercontent.com/proxy/53q9lxniFE1MzZJrWOTu92467VD712LCNxVpoQIL7Gc0bgE92DXh8-hKsixBmCFnjVHj2Ru4w_29Pc3ZacD4l-RH5ck\" height=\"200\" width=\"auto\" /><br><br>パサージュ",
+                "description": "<img src=\"https://lh3.googleusercontent.com/proxy/53q9lxniFE1MzZJrWOTu92467VD712LCNxVpoQIL7Gc0bgE92DXh8-hKsixBmCFnjVHj2Ru4w_29Pc3ZacD4l-RH5ck\" height=\"200\" width=\"auto\" />",
                 "gx_media_links": "https://lh3.googleusercontent.com/proxy/53q9lxniFE1MzZJrWOTu92467VD712LCNxVpoQIL7Gc0bgE92DXh8-hKsixBmCFnjVHj2Ru4w_29Pc3ZacD4l-RH5ck"
             }
         },
@@ -204,7 +204,7 @@ let locations = {
                     "highlight": "#icon-125-highlight"
                 },
                 "icon": "https://www.gstatic.com/mapspro/images/stock/125-restaurant.png",
-                "description": "<img src=\"https://lh6.googleusercontent.com/proxy/2iN4yygWVaY8ivHQoB4g5PqFCF2qthJYhoK44dMhKiDfa-TXrhLqM1-MbCq-pn2gsHIi842vNA0jnlxQ7pmL8CZE-jl7WA\" height=\"200\" width=\"auto\" /><br><br>Angepatio",
+                "description": "<img src=\"https://lh6.googleusercontent.com/proxy/2iN4yygWVaY8ivHQoB4g5PqFCF2qthJYhoK44dMhKiDfa-TXrhLqM1-MbCq-pn2gsHIi842vNA0jnlxQ7pmL8CZE-jl7WA\" height=\"200\" width=\"auto\" />",
                 "gx_media_links": "https://lh6.googleusercontent.com/proxy/2iN4yygWVaY8ivHQoB4g5PqFCF2qthJYhoK44dMhKiDfa-TXrhLqM1-MbCq-pn2gsHIi842vNA0jnlxQ7pmL8CZE-jl7WA"
             }
         },
@@ -227,7 +227,7 @@ let locations = {
                     "highlight": "#icon-125-highlight"
                 },
                 "icon": "https://www.gstatic.com/mapspro/images/stock/125-restaurant.png",
-                "description": "<img src=\"https://lh4.googleusercontent.com/proxy/iKmFcXNFB_tCsEyEqNSCZHaldDA2DZtehwjqSepB_bqGypFjIv8y3Glhb32r1O-BbalNWf1lJoBTl9mCz3SIa8Gw8Q\" height=\"200\" width=\"auto\" /><br><br>哲ちゃん",
+                "description": "<img src=\"https://lh4.googleusercontent.com/proxy/iKmFcXNFB_tCsEyEqNSCZHaldDA2DZtehwjqSepB_bqGypFjIv8y3Glhb32r1O-BbalNWf1lJoBTl9mCz3SIa8Gw8Q\" height=\"200\" width=\"auto\" />",
                 "gx_media_links": "https://lh4.googleusercontent.com/proxy/iKmFcXNFB_tCsEyEqNSCZHaldDA2DZtehwjqSepB_bqGypFjIv8y3Glhb32r1O-BbalNWf1lJoBTl9mCz3SIa8Gw8Q"
             }
         },
@@ -250,7 +250,7 @@ let locations = {
                     "highlight": "#icon-125-highlight"
                 },
                 "icon": "https://www.gstatic.com/mapspro/images/stock/125-restaurant.png",
-                "description": "<img src=\"https://seralulalu.files.wordpress.com/2010/02/11-10-2009_22-58-48.jpg\" height=\"200\" width=\"auto\" /><br><br>焼肉レストラン雪月花",
+                "description": "<img src=\"https://seralulalu.files.wordpress.com/2010/02/11-10-2009_22-58-48.jpg\" height=\"200\" width=\"auto\" />",
                 "gx_media_links": "https://seralulalu.files.wordpress.com/2010/02/11-10-2009_22-58-48.jpg"
             }
         },
@@ -273,7 +273,7 @@ let locations = {
                     "highlight": "#icon-35-highlight"
                 },
                 "icon": "https://www.gstatic.com/mapspro/images/stock/35-coffeehouse.png",
-                "description": "<img src=\"https://i1.wp.com/imageshack.com/a/img909/7845/GCCGuc.jpg\" height=\"200\" width=\"auto\" /><br><br>まんが帝国 恵比寿店",
+                "description": "<img src=\"https://i1.wp.com/imageshack.com/a/img909/7845/GCCGuc.jpg\" height=\"200\" width=\"auto\" />",
                 "gx_media_links": "https://i1.wp.com/imageshack.com/a/img909/7845/GCCGuc.jpg"
             }
         },
@@ -296,8 +296,8 @@ let locations = {
                     "highlight": "#icon-35-highlight"
                 },
                 "icon": "https://www.gstatic.com/mapspro/images/stock/35-coffeehouse.png",
-                "description": "<img src=\"https://lh6.googleusercontent.com/proxy/Sau6bWKXKjzVuKEHaC2xbGRaqcITZb9b8y5u-9EySPqMjYTHgzFt1zFy8MaV_A_bjir3zvBn_8jDxGoGBDJjdLBBctCZTzeimPgPjWfM-w\" height=\"200\" width=\"auto\" /><br><br>",
-                "gx_media_links": "https://lh6.googleusercontent.com/proxy/Sau6bWKXKjzVuKEHaC2xbGRaqcITZb9b8y5u-9EySPqMjYTHgzFt1zFy8MaV_A_bjir3zvBn_8jDxGoGBDJjdLBBctCZTzeimPgPjWfM-w"
+                "description": "<img src=\"https://maps.googleapis.com/maps/api/place/js/PhotoService.GetPhoto?1sCmRaAAAA2dIrFsKH557ss4MBH4PF4eLouIzgoa0krOyfe2NZHzHK6yySgMCCQpjWKQwEtvhZc5D_iIYeY8DvbPuGbirghsMmijsneFdgk61hPa9Dxk2Cv5Sqsl6h0uocpbjw6TkyEhCFPBf9QO4TUwkAS_TlGrneGhThVzGQyDC_c-sRIf0q39UicF5Cow&4u16383&5m1&2e1&callback=none&client=google-maps-pro&token=77526\" /><br><br>",
+                "gx_media_links": "https://maps.googleapis.com/maps/api/place/js/PhotoService.GetPhoto?1sCmRaAAAA2dIrFsKH557ss4MBH4PF4eLouIzgoa0krOyfe2NZHzHK6yySgMCCQpjWKQwEtvhZc5D_iIYeY8DvbPuGbirghsMmijsneFdgk61hPa9Dxk2Cv5Sqsl6h0uocpbjw6TkyEhCFPBf9QO4TUwkAS_TlGrneGhThVzGQyDC_c-sRIf0q39UicF5Cow&4u16383&5m1&2e1&callback=none&client=google-maps-pro&token=77526"
             }
         },
         {
@@ -319,7 +319,7 @@ let locations = {
                     "highlight": "#icon-158-highlight"
                 },
                 "icon": "https://www.gstatic.com/mapspro/images/stock/158-yellow.png",
-                "description": "<img src=\"https://lh5.googleusercontent.com/proxy/J2413qUSVUIpXkQyla-XLeT9TtnjNke8s67N_VqehAB3140-LdF7z_aWiuqtGD33XtXtxqgJB_xeJknMv8xy88Z77sZIZf9hNtpVivWobWYjBPAF6-uy6-jcEXKSAa7fWm4e\" height=\"200\" width=\"auto\" /><br><br>ラ・フェット多摩 南大沢",
+                "description": "<img src=\"https://lh5.googleusercontent.com/proxy/J2413qUSVUIpXkQyla-XLeT9TtnjNke8s67N_VqehAB3140-LdF7z_aWiuqtGD33XtXtxqgJB_xeJknMv8xy88Z77sZIZf9hNtpVivWobWYjBPAF6-uy6-jcEXKSAa7fWm4e\" height=\"200\" width=\"auto\" />",
                 "gx_media_links": "https://lh5.googleusercontent.com/proxy/J2413qUSVUIpXkQyla-XLeT9TtnjNke8s67N_VqehAB3140-LdF7z_aWiuqtGD33XtXtxqgJB_xeJknMv8xy88Z77sZIZf9hNtpVivWobWYjBPAF6-uy6-jcEXKSAa7fWm4e"
             }
         },
@@ -342,7 +342,7 @@ let locations = {
                     "highlight": "#icon-158-highlight"
                 },
                 "icon": "https://www.gstatic.com/mapspro/images/stock/158-yellow.png",
-                "description": "<img src=\"https://media.timeout.com/images/105386766/image.jpg\" height=\"200\" width=\"auto\" /><br><br>WEDGWOODのティーカップ＆ソーサー",
+                "description": "<img src=\"https://media.timeout.com/images/105386766/image.jpg\" height=\"200\" width=\"auto\" />",
                 "gx_media_links": "https://media.timeout.com/images/105386766/image.jpg"
             }
         },
@@ -365,7 +365,7 @@ let locations = {
                     "highlight": "#icon-145-highlight"
                 },
                 "icon": "https://www.gstatic.com/mapspro/images/stock/145-tree.png",
-                "description": "<img src=\"https://lh4.googleusercontent.com/proxy/WOS2tGf9caiccVAGtulw41VvbEpT1DdhbkcVPNsyGJ4QJOhwSXJe1Fe_BwpCWJeqOQMx4cWL55ztQwDtxdDt62lH6iLzehlRXUiDiA\" height=\"200\" width=\"auto\" /><br><br>仙台堀川公園",
+                "description": "<img src=\"https://lh4.googleusercontent.com/proxy/WOS2tGf9caiccVAGtulw41VvbEpT1DdhbkcVPNsyGJ4QJOhwSXJe1Fe_BwpCWJeqOQMx4cWL55ztQwDtxdDt62lH6iLzehlRXUiDiA\" height=\"200\" width=\"auto\" />",
                 "gx_media_links": "https://lh4.googleusercontent.com/proxy/WOS2tGf9caiccVAGtulw41VvbEpT1DdhbkcVPNsyGJ4QJOhwSXJe1Fe_BwpCWJeqOQMx4cWL55ztQwDtxdDt62lH6iLzehlRXUiDiA"
             }
         },
@@ -388,7 +388,7 @@ let locations = {
                     "highlight": "#icon-145-highlight"
                 },
                 "icon": "https://www.gstatic.com/mapspro/images/stock/145-tree.png",
-                "description": "<img src=\"https://lh4.googleusercontent.com/proxy/TcN8JcKgmjX_Wmgj1mB0FWlTQk0G9rb7yh_fxGvEbfbg4gQRGLE4u4HC6MqTa8wd_3o2CYpw0N7E-iw-Shg0tWEmorpRjQm5j-8\" height=\"200\" width=\"auto\" /><br><br>御成橋公園",
+                "description": "<img src=\"https://lh4.googleusercontent.com/proxy/TcN8JcKgmjX_Wmgj1mB0FWlTQk0G9rb7yh_fxGvEbfbg4gQRGLE4u4HC6MqTa8wd_3o2CYpw0N7E-iw-Shg0tWEmorpRjQm5j-8\" height=\"200\" width=\"auto\" />",
                 "gx_media_links": "https://lh4.googleusercontent.com/proxy/TcN8JcKgmjX_Wmgj1mB0FWlTQk0G9rb7yh_fxGvEbfbg4gQRGLE4u4HC6MqTa8wd_3o2CYpw0N7E-iw-Shg0tWEmorpRjQm5j-8"
             }
         },
@@ -411,7 +411,7 @@ let locations = {
                     "highlight": "#icon-145-highlight"
                 },
                 "icon": "https://www.gstatic.com/mapspro/images/stock/145-tree.png",
-                "description": "<img src=\"https://media.timeout.com/images/105528109/750/422/image.jpg\" height=\"200\" width=\"auto\" /><br><br>",
+                "description": "<img src=\"https://media.timeout.com/images/105528109/750/422/image.jpg\" height=\"200\" width=\"auto\" />",
                 "gx_media_links": "https://media.timeout.com/images/105528109/750/422/image.jpg"
             }
         },
@@ -434,7 +434,7 @@ let locations = {
                     "highlight": "#icon-112-highlight"
                 },
                 "icon": "https://www.gstatic.com/mapspro/images/stock/112-purple.png",
-                "description": "<img src=\"https://lh4.googleusercontent.com/proxy/sP7yVusd3F8gsHu4dZqSrhOYZDbfKYEutd0xZBWvaYiwtZF_0l5lkpu10qw_BuMklakOoAzs8PZUJSFgN_d4iOGL\" height=\"200\" width=\"auto\" /><br><br>closed in 2009",
+                "description": "<img src=\"https://lh4.googleusercontent.com/proxy/sP7yVusd3F8gsHu4dZqSrhOYZDbfKYEutd0xZBWvaYiwtZF_0l5lkpu10qw_BuMklakOoAzs8PZUJSFgN_d4iOGL\" height=\"200\" width=\"auto\" />",
                 "gx_media_links": "https://lh4.googleusercontent.com/proxy/sP7yVusd3F8gsHu4dZqSrhOYZDbfKYEutd0xZBWvaYiwtZF_0l5lkpu10qw_BuMklakOoAzs8PZUJSFgN_d4iOGL"
             }
         },
@@ -457,7 +457,7 @@ let locations = {
                     "highlight": "#icon-105-highlight"
                 },
                 "icon": "https://www.gstatic.com/mapspro/images/stock/105-police.png",
-                "description": "<img src=\"https://lh4.googleusercontent.com/proxy/SXCe24lDgJc9zLsObYcfOR2BfyVaiJVwxjQpWR_hOieg5Iu0bqdbzmtaPwYAGpvg1Tbne-H0SqQMauRJma59NqvaaYsAFyo\" height=\"200\" width=\"auto\" /><br><br>希望ケ丘自治会館",
+                "description": "<img src=\"https://lh4.googleusercontent.com/proxy/SXCe24lDgJc9zLsObYcfOR2BfyVaiJVwxjQpWR_hOieg5Iu0bqdbzmtaPwYAGpvg1Tbne-H0SqQMauRJma59NqvaaYsAFyo\" height=\"200\" width=\"auto\" />",
                 "gx_media_links": "https://lh4.googleusercontent.com/proxy/SXCe24lDgJc9zLsObYcfOR2BfyVaiJVwxjQpWR_hOieg5Iu0bqdbzmtaPwYAGpvg1Tbne-H0SqQMauRJma59NqvaaYsAFyo"
             }
         }
